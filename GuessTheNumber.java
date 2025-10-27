5d3f5b1425d3179e9a456abbf205c08fd2ae7505
Sai Manogna Sri <saimanognasri.k@gmail.com>
2025-10-27 16:25:45 +0530

    Format GuessTheNumber.java for better readability

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -16,37 +16,37 @@ public class GuessTheNumber {
             int secretNumber = random.nextInt(100) + 1;
             int maxGuesses = 5;
             boolean guessed = false;
-
             for (int attempt = 1; attempt <= maxGuesses; attempt++) {
                 System.out.print("Guess #" + attempt + ": ");
                 int userGuess = scanner.nextInt();
-
-                if (userGuess == secretNumber) {
+                if (userGuess == secretNumber) 
+                {
                     System.out.println("Congratulations! You guessed it right!");
                     guessed = true;
                     break;
-                } else if (attempt == maxGuesses - 1) {
+                } 
+                else if (attempt == maxGuesses - 1) 
+                {
                     int lowerRange = Math.max(1, secretNumber - 2);
                     int upperRange = Math.min(100, secretNumber + 2);
                     System.out.println("Major Hint! The number is between " + lowerRange + " and " + upperRange + ".");
-                } else if (userGuess < secretNumber) {
-                    System.out.println("Go higher!");
-                } else {
-                    System.out.println("Go lower!");
-                }
+                } 
+                else if (userGuess < secretNumber) 
+                {System.out.println("Go higher!");} 
+                else 
+                {System.out.println("Go lower!");}
             }
 
-            if (!guessed) {
-                System.out.println("\nOut of guesses! The correct number was " + secretNumber + ".");
-            }
+            if (!guessed) 
+            {System.out.println("\nOut of guesses! The correct number was " + secretNumber + ".");}
 
             System.out.print("\nWould you like to play again? (yes/no): ");
             String response = scanner.next().trim().toLowerCase();
             playAgain = response.equals("yes");
             System.out.println();
         }
-
         System.out.println("Thanks for playing! See you next time!");
         scanner.close();
     }
 }
+
